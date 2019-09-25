b03ae3566a71bc2cd38ce846d16d70f0516487a5
Nikita Topchii <nikita.topchiy@gmail.com>
2019-09-25 21:44:44 +0300

    First commit

---
+++ CP_LAB1/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+void initMap(map<char, int>::iterator it) {
+
+
+};
+int main() {
+	SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
+	char a = char(437);
+	cout << a << endl;
+	cout << "h" << endl;
+	//map <char, int> LetterAmount;
+	system("pause");
+}
